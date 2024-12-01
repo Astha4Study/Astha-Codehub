748d5493974d7802718eef2c9f74f4a8de7f2240
Astha4Study <iyandabes1@gmail.com>
2024-12-02 00:10:35 +0700

    adding responsive, and animation

---
+++ public/script.js
@@ -0,0 +1,9 @@
+// Scroll Reveal
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.aboutme-content', { origin: 'left' });
+ScrollReveal().reveal('.profile-img', { origin: 'right' });
